b770525721ee3297b555747b137ad515e5f5e0e0
Keira Dueck <sylae@calref.net>
2021-01-06 06:18:32 -0800

    update copyright year

--- config.sample.php
+++ config.sample.php
@@ -1,12 +1,14 @@
 <?php
 
 /*
- * Copyright (c) 2020 Keira Dueck <sylae@calref.net>
+ * Copyright (c) 2021 Keira Dueck <sylae@calref.net>
  * Use of this source code is governed by the MIT license, which
  * can be found in the LICENSE file.
  */
 
 namespace Huntress;
+
+use Monolog\Logger;
 
 $config = [];
 
@@ -17,7 +19,7 @@ $config['database'] = "mysql://huntress:PASSWORD@localhost/huntress";
 $config['botToken'] = "xxx";
 
 // how much logging you want on the console.
-$config['logLevel'] = \Monolog\Logger::INFO;
+$config['logLevel'] = Logger::INFO;
 
 // Discord IDs of users who you are comfortable being able to execute arbitary code on your machine.
 $config['evalUsers'] = [
